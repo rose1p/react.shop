b418d1462e7878dd697b7c7706ce2168ca205b65
rose1p <rose1p@naver.com>
2023-10-31 16:20:40 +0900

    ver07

--- back/routes/users.js
+++ back/routes/users.js
@@ -1,6 +1,34 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../db');
+var multer = require('multer');
+
+//사진 업로드
+var upload = multer({
+    storage: multer.diskStorage({
+        destination: (req, file, done) => {
+            done(null, './public/upload/photo')
+        },
+        filename: (req, file, done) => {
+            var fileName = Date.now() + '.jpg';
+            done(null, fileName);
+        }
+    })
+});
+
+//사진 업로드
+router.post('/update/photo', upload.single('file'), function (req, res) {
+    const filename = '/upload/photo/' + req.file.filename;
+    const uid = req.body.uid;
+    const sql = 'update users set photo=? where uid=?';
+    db.get().query(sql, [filename, uid], function (err) {
+        if (err) {
+            res.send('0');
+        } else {
+            res.send('1');
+        }
+    });
+});
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -14,13 +42,13 @@ router.post('/login', function (req, res) {
     const upass = req.body.upass;
     const sql = 'select * from users where uid=?';
     db.get().query(sql, [uid], function (err, rows) {
-        if(rows.length > 0) {
-            if(rows[0].upass == upass){
+        if (rows.length > 0) {
+            if (rows[0].upass == upass) {
                 res.send('1');
-            }else{
+            } else {
                 res.send('2');
             }
-        }else{
+        } else {
             res.send('0');
         }
     });
@@ -28,16 +56,16 @@ router.post('/login', function (req, res) {
 
 
 //사용자정보읽기 REST API
-router.get('/read/:uid', function(req, res){ //localhost:5000/users/read/blue
-    const uid=req.params.uid;
-    const sql='select *, date_format(regdate,"%Y-%m-%d %T") fmtdate from users where uid=?';
-    db.get().query(sql, [uid], function(err, rows){
+router.get('/read/:uid', function (req, res) { //localhost:5000/users/read/blue
+    const uid = req.params.uid;
+    const sql = 'select *, date_format(regdate,"%Y-%m-%d %T") fmtdate from users where uid=?';
+    db.get().query(sql, [uid], function (err, rows) {
         res.send(rows[0]);
     });
 });
 
 //사용자 정보 수정
-router.post('/update', function(req, res) {
+router.post('/update', function (req, res) {
     const uname = req.body.uname;
     const phone = req.body.phone;
     const address1 = req.body.address1;
@@ -45,11 +73,11 @@ router.post('/update', function(req, res) {
     const uid = req.body.uid;
     const sql = 'update users set uname=?, phone=?, address1=?, address2=? where uid=?';
     console.log(uname, phone, address1, address2, uid);
-    db.get().query(sql, [uname, phone, address1, address2, uid], function(err) {
+    db.get().query(sql, [uname, phone, address1, address2, uid], function (err) {
         console.log('.................', err);
-        if(err) {
+        if (err) {
             res.send('0');
-        }else {
+        } else {
             res.send('1');
         }
     });
